ea7b34615696adc5108eeef7f67803587e2555cc
Christopher D. Canfield <cdcanfield@gmail.com>
2021-05-04 11:56:36 -0400

    Simplified the DeepWater sprite.
    This fixes the lines that can occur between deep water sprites when the window is resized. The tiled version of the deep water sprite was replaced with a single texture, for now.

--- bubolo/src/bubolo/graphics/DeepWaterSprite.java
+++ bubolo/src/bubolo/graphics/DeepWaterSprite.java
@@ -1,21 +1,20 @@
 package bubolo.graphics;
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.Texture;
 
 import bubolo.world.DeepWater;
 
 /**
- * The graphical representation of a DeepWater.
+ * DeepWater sprites should only be placed next to Water tiles, because they are not designed to match up with other tiles.
  *
  * @author BU673 - Clone Industries
  */
 class DeepWaterSprite extends AbstractEntitySprite<DeepWater>
 {
-	// list of texture regions, used for different tiling states
-	private TextureRegion[] frames;
-
-	/** The file name of the texture. */
+	/** The texture's file name. */
 	private static final String TEXTURE_FILE = "deepwater.png";
+	
+	private final Texture texture;
 
 	/**
 	 * Constructor for the DeepWaterSprite. This is Package-private because sprites should not be
@@ -28,74 +27,16 @@ class DeepWaterSprite extends AbstractEntitySprite<DeepWater>
 	{
 		super(DrawLayer.TERRAIN, deepWater);
 
-		frames = Graphics.getTextureRegion1d(TEXTURE_FILE, getClass());
+		texture = Graphics.getTexture(TEXTURE_FILE);
 	}
 
 	@Override
 	public void draw(Graphics graphics)
 	{
-
-		int currentState = this.getEntity().getTilingState();
-
-		if (isDisposed())
-		{
+		if (isDisposed()) {
 			graphics.sprites().removeSprite(this);
-		}
-		else
-		{
-			drawTexture(graphics, frames[currentState]);
-		}
-
-		boolean[] corners = this.getEntity().getCornerMatches();
-
-		if (currentState == 0 || currentState == 13 || currentState == 5 || currentState == 7)
-		{
-			if (!corners[0])
-			{
-				drawTexture(graphics, frames[20]);
-			}
-			else
-			{
-				drawTexture(graphics, frames[16]);
-			}
-		}
-
-		if (currentState == 0 || currentState == 11 || currentState == 9 || currentState == 13)
-		{
-			if (!corners[1])
-			{
-				drawTexture(graphics, frames[21]);
-			}
-			else
-			{
-				drawTexture(graphics, frames[17]);
-			}
-
-		}
-
-		if (currentState == 0 || currentState == 14 || currentState == 6 || currentState == 7)
-		{
-			if (!corners[2])
-			{
-				drawTexture(graphics, frames[22]);
-			}
-			else
-			{
-				drawTexture(graphics, frames[18]);
-			}
-
-		}
-
-		if (currentState == 0 || currentState == 10 || currentState == 14 || currentState == 11)
-		{
-			if (!corners[3])
-			{
-				drawTexture(graphics, frames[23]);
-			}
-			else
-			{
-				drawTexture(graphics, frames[19]);
-			}
+		} else {
+			drawTexture(graphics, texture);
 		}
 	}
 }
